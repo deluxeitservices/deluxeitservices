a869417cdfe6e666e3d8f1517a282f349de89257
juned <junedm90@gmail.com>
2025-04-09 23:32:07 +0100

    push info file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,8 +16,7 @@
             <img src="assets/img/heronew.png" class="img-fluid animated" alt="">
           </div>
           <div class="col-lg-6  d-flex flex-column justify-content-center" data-aos="fade-in">
-            <h1>Make Your World Work<span> Deluxe IT Services</span></h1>
-
+            <h1>Your Business, Our Technology — Empowered by<span> Deluxe IT Services</span></h1>
             <p>At Deluxe IT Services, we specialize in cutting-edge IT solutions, including expert PHP and Laravel development. Our dedicated team ensures innovation, efficiency, and tailored solutions to elevate your business.</p>
             <div class="d-flex">
               <a href="#about" class="btn-get-started">Get Started</a>
